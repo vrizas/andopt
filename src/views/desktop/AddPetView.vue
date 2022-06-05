711704743de7b5e8fef65f3167ba9fea36919675
vrizas <vrizasizza@gmail.com>
2022-06-05 16:47:42 +0700

    create pet view

---
+++ src/views/desktop/AddPetView.vue
@@ -0,0 +1,95 @@
+<script setup>
+</script>
+
+<template>
+  <main class="py-8 px-10 flex gap-5 bg-white">
+    <h2 class="text-black text-xl font-semibold my-5">Unggah Hewan Peliharaan</h2>
+    <aside class="flex justify-center items-center bg-lightGray rounded w-60 h-80 w-1/4" >
+      <h2 class="text-black font-semibold">Unggah Poto Hewan</h2>
+    </aside>
+    <section>
+      <form class="flex flex-col gap-3 text-sm w-3/4"></form>
+      <div class="flex flex-col gap-1">
+          <label for="petname">Nama Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petname"
+            id="petname"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="petcategory">Kategori Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petcategory"
+            id="petcategory"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="pettype">Jenis Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="pettype"
+            id="pettype"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="petage">Umur Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petage"
+            id="petage"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="petgender">Jenis Kelamin</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petgender"
+            id="petgender"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="petcondition">Kondisi Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petcondition"
+            id="petcondition"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-6">
+          <label for="petdescription">Deskripsi Hewan</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petdescription"
+            id="petdescription"
+            required
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="petloc">Lokasi</label>
+          <input
+            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
+            type="text"
+            v-model="petloc"
+            id="petloc"
+            required
+          />
+        </div>
+    </section>
+
+  </main>
+</template>
--- src/views/desktop/PetView.vue
+++ src/views/desktop/PetView.vue
@@ -2,94 +2,50 @@
 </script>
 
 <template>
-  <main class="py-8 px-10 flex gap-5 bg-white">
-    <h2 class="text-black text-xl font-semibold my-5">Unggah Hewan Peliharaan</h2>
-    <aside class="flex justify-center items-center bg-lightGray rounded w-60 h-80 w-1/4" >
-      <h2 class="text-black font-semibold">Unggah Poto Hewan</h2>
-    </aside>
+  <main class="py-8 px-10 flex gap-10 bg-white min-h-[70vh]">
     <section>
-      <form class="flex flex-col gap-3 text-sm w-3/4"></form>
-      <div class="flex flex-col gap-1">
-          <label for="petname">Nama Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petname"
-            id="petname"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="petcategory">Kategori Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petcategory"
-            id="petcategory"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="pettype">Jenis Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="pettype"
-            id="pettype"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="petage">Umur Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petage"
-            id="petage"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="petgender">Jenis Kelamin</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petgender"
-            id="petgender"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="petcondition">Kondisi Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petcondition"
-            id="petcondition"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-6">
-          <label for="petdescription">Deskripsi Hewan</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petdescription"
-            id="petdescription"
-            required
-          />
-        </div>
-        <div class="flex flex-col gap-1">
-          <label for="petloc">Lokasi</label>
-          <input
-            class="py-3 px-4 bg-white border-b-2 border-primary text-sm"
-            type="text"
-            v-model="petloc"
-            id="petloc"
-            required
-          />
-        </div>
+      <img src="../../assets/images/cat.jpg" alt="cat" class="w-72 h-72 object-cover rounded-lg">
     </section>
-
+    <section class="w-1/2">
+      <h2 class="font-bold text-xl">
+        Ardi
+        <font-awesome-icon icon="mars" class="text-darkGray text-2xl ml-3" />
+      </h2>
+      <p class="mt-2">Kucing Persia</p>
+      <p class="font-medium text-darkGray mt-1">2 tahun</p>
+      <p class="mt-3">
+        <font-awesome-icon
+          icon="location-dot"
+          class="text-primary mr-1"
+        />
+        Jakarta
+      </p>
+      <div class="mt-8">
+        <h3 class="font-medium text-lg text-darkGray mb-2">Deskripsi</h3>
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati porro officiis illo cupiditate fuga tempora amet eum, officia, natus adipisci ratione. Amet sint velit nesciunt itaque enim qui quasi non fugit. Accusamus blanditiis quas exercitationem eaque, quam quidem, cupiditate perferendis autem quaerat vel architecto ut suscipit delectus consectetur molestiae incidunt!</p>
+      </div>
+    </section>
+    <aside class="shadow-andopt w-1/4 py-8 px-6 rounded-lg h-fit">
+      <h4 class="text-xs text-medium text-darkGray mb-2">Pemilik</h4>
+      <a href="#" class="flex gap-2 items-center font-semibold truncate">
+          <img src="../../assets/images/default-profile-pic.png" alt="profil" class="w-8 h-8 rounded-full object-cover" draggable="false">
+          vrizas
+      </a>
+      <hr class="my-4 text-lightGray">
+      <div class="flex flex-col gap-3">
+        <button class="bg-primary text-white font-semibold text-sm py-1 px-5 w-full rounded-md">
+          <font-awesome-icon icon="comment-dots" class="mr-1" />
+          Chat
+        </button>
+        <button class="bg-secondary text-white font-semibold text-sm py-1 px-5 w-full rounded-md">
+          <font-awesome-icon icon="paw" class="mr-1" />
+          Adopsi
+        </button>
+        <button class="border border-pink text-pink hover:bg-pink hover:text-white font-semibold text-sm py-1 px-5 w-full rounded-md">
+          <font-awesome-icon icon="heart" class="mr-1"/>
+          Sukai
+        </button>
+      </div>
+    </aside>
   </main>
 </template>
